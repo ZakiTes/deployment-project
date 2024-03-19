c143a6e6914701fe7bab4b98c7ef6733d7b3465e
ZakiTes <zakites13@outlook.com>
2024-03-19 21:04:23 +0100

    updated endpoints

--- routes/pictures.js
+++ routes/pictures.js
@@ -8,6 +8,18 @@ router.get('/', function(req, res, next) {
     res.render('pictures', { pictures: pictures});
 });
 
+router.get('/:pictureName', function(req, res, next) {
+    const picture = req.params.pictureName;
+    const picturePath = path.join(__dirname, '../pictures/', picture);
+
+    fs.access(picturePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            return res.status(404).send('Picture not found');
+        }
+        res.render('pictures', {pictures: [picture], picture: picture });
+    });
+});
+
 
 router.post('/', function(req, res, next) {
     const file = req.files.file;
